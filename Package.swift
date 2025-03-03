c089d286ffda93c0fd43dd062287bad45e8bb847
Izza Khawar <izza.khawar@asappstudio.com>
2025-03-03 14:38:22 +0500

    public

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "SpeedometerSwiftUI")
+            name: "SpeedometerSwiftUI",
+            path: "Sources")
     ]
 )
